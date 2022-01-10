28298a407d7ebc01bc06267b981d263ac573f876
sergeibogko <69677370+sergeibogko@users.noreply.github.com>
2022-01-10 16:56:29 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -6,3 +6,16 @@ $(".menu__list-link").on("click", function () {
   $(".menu, .header__btn").removeClass("active");
   $("body").removeClass("lock");
 });
+
+var $page = $("html, body");
+$('a[href*="#"]').click(function () {
+  $page.animate(
+    {
+      scrollTop: $($.attr(this, "href")).offset().top,
+    },
+    1500
+  );
+  return false;
+});
+
+var mixer = mixitup(".portfolio__gallery");
