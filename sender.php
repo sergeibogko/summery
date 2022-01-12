ff1e3a8d185ba6244211892d3ae50e394dd55684
sergeibogko <69677370+sergeibogko@users.noreply.github.com>
2022-01-12 15:26:43 +0200

    Add files via upload

---
+++ sender.php
@@ -0,0 +1,21 @@
+<?php
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $text = $_POST['text'];
+
+	$to = "sergeibogko85a@gmail.com"; 
+	$date = date ("d.m.Y"); 
+	$time = date ("h:i");
+	$from = $email;
+	$subject = "Заявка c сайта";
+
+	
+	$msg="
+    Имя: $name /n
+    Почта: $email /n
+    Текст: $text"; 	
+	mail($to, $subject, $msg, "From: $from ");
+
+?>
+
+<p>Привет, форма отправлена</p>
