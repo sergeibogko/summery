3ff5110638b525fe0e4ca17bfa5e8a5ff9a626d9
sergeibogko <69677370+sergeibogko@users.noreply.github.com>
2022-01-13 15:08:38 +0200

    Add files via upload

--- sender.php
+++ sender.php
@@ -1,21 +1,25 @@
 <?php
+if(isset($_SERVER['HTTP_X_REQUESTED_WITH']) && !empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') //проверка на асинхронность
+	{
+    if (isset($_POST["name"]) && isset($_POST["email"])) 
+	{ 
+    if ($_POST['name'] == '') 
+    {
+        echo 'Не заполнено поле Имя';
+        return; //проверяем наличие обязательных полей
+    }
+    if ($_POST['email'] == '') 
+    {
+        echo 'Не заполнено поле E-mail';
+        return;
+    } 
     $name = $_POST['name'];
     $email = $_POST['email'];
-    $text = $_POST['text'];
+	$text = $_POST['text'];
 
-	$to = "sergeibogko85@gmail.com"; 
-	$date = date ("d.m.Y"); 
-	$time = date ("h:i");
-	$from = $email;
-	$subject = "Заявка c сайта";
-
-	
-	$msg="
-    Имя: $name /n
-    Почта: $email /n
-    Текст: $text"; 	
-	mail($to, $subject, $msg, "From: $to ");
-
+mail("email1@yandex.ru", "Заявка с сайта", "Имя:".$name.". E-mail: ".$email." "Тукст:".$text ," From: email2@yandex.ru \r\n"); //здесь делаем отправку заявки на почту. не забудьте поменять оба адреса
+        echo 'Заявка отправлена!';
+    	return; //возвращаем сообщение пользователю
+    }
+    }
 ?>
-
-<p>Привет, форма отправлена</p>
